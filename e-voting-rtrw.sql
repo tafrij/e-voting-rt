e74863e3ba9f433d0e8c237b98d2bc7eaa8f06e9
rido-alfa <rido-alfa@github.com>
2021-08-09 23:12:18 +0700

    update database

--- e-voting-rtrw.sql
+++ e-voting-rtrw.sql
@@ -1,108 +1,110 @@
--- MySQL dump 10.13  Distrib 8.0.26, for Linux (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: e-voting-rtrw
--- ------------------------------------------------------
--- Server version	8.0.26-0ubuntu0.20.04.2
+-- Host: localhost:3306
+-- Generation Time: Aug 09, 2021 at 11:11 PM
+-- Server version: 8.0.26-0ubuntu0.20.04.2
+-- PHP Version: 7.2.34-23+ubuntu20.04.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `e-voting-rtrw`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `kandidat`
 --
 
-DROP TABLE IF EXISTS `kandidat`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `kandidat` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL,
   `no_kandidat` int NOT NULL,
   `nama` varchar(200) NOT NULL,
   `visi` text NOT NULL,
   `misi` text NOT NULL,
-  `image` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `kandidat`
 --
 
-LOCK TABLES `kandidat` WRITE;
-/*!40000 ALTER TABLE `kandidat` DISABLE KEYS */;
-INSERT INTO `kandidat` VALUES (4,94,'Magna voluptatem ci','Consequatur quidem','Quae soluta in elige','kades-21.png'),(5,58,'Duis magni aut itaqu','Est ratione magni et','Explicabo Illum et','kades-11.png');
-/*!40000 ALTER TABLE `kandidat` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `kandidat` (`id`, `no_kandidat`, `nama`, `visi`, `misi`, `image`) VALUES
+(4, 4, 'Magna voluptatem ci', 'Consequatur quidem', 'Quae soluta in elige', 'kades-21.png'),
+(5, 5, 'Duis magni aut itaqu', 'Est ratione magni et', 'Explicabo Illum et', 'kades-11.png');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL,
   `name` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `username` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `image` varchar(128) NOT NULL,
   `password` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `role_id` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `role_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `user`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (13,'Administrator','admin','foto.jpeg','admin',1),(41,'Vel libero ea impedi','velliberoeaimpedi','default.jpg',NULL,2),(42,'Dolores ad eos nost','doloresadeosnost','default.jpg',NULL,2),(43,'Ab ea quo in deserun','abeaquoindeserun','default.jpg',NULL,3),(44,'Consequuntur aut omn','consequunturautomn','default.jpg',NULL,2);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `user` (`id`, `name`, `username`, `image`, `password`, `role_id`) VALUES
+(13, 'Administrator', 'admin', 'foto.jpeg', 'admin', 1),
+(41, 'Vel libero ea impedi', 'velliberoeaimpedi', 'default.jpg', NULL, 2),
+(42, 'Dolores ad eos nost', 'doloresadeosnost', 'default.jpg', NULL, 2),
+(43, 'Ab ea quo in deserun', 'abeaquoindeserun', 'default.jpg', NULL, 3),
+(44, 'Consequuntur aut omn', 'consequunturautomn', 'default.jpg', NULL, 2),
+(45, 'Illo exercitation ei', 'illoexercitationei', 'default.jpg', NULL, 2),
+(46, 'Enim est consequuntu', 'enimestconsequuntu', 'default.jpg', NULL, 2),
+(47, 'Est doloribus ut pro', 'estdoloribusutpro', 'default.jpg', NULL, 2),
+(48, 'Excepteur voluptates', 'excepteurvoluptates', 'default.jpg', NULL, 2),
+(49, 'Repellendus Invento', 'repellendusinvento', 'default.jpg', NULL, 2),
+(50, 'Vel qui magni iusto ', 'velquimagniiusto', 'default.jpg', NULL, 2);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `user_access_menu`
 --
 
-DROP TABLE IF EXISTS `user_access_menu`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_access_menu` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL,
   `role_id` int NOT NULL,
-  `menu_id` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `menu_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `user_access_menu`
 --
 
-LOCK TABLES `user_access_menu` WRITE;
-/*!40000 ALTER TABLE `user_access_menu` DISABLE KEYS */;
-INSERT INTO `user_access_menu` VALUES (1,1,1),(3,2,2),(7,1,3),(8,1,2);
-/*!40000 ALTER TABLE `user_access_menu` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
+(1, 1, 1),
+(3, 2, 2),
+(7, 1, 3),
+(8, 1, 2);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `user_detail`
 --
 
-DROP TABLE IF EXISTS `user_detail`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_detail` (
   `user_id` int NOT NULL,
   `nik` int NOT NULL,
@@ -113,130 +115,213 @@ CREATE TABLE `user_detail` (
   `pekerjaan` varchar(100) NOT NULL,
   `vote_status` int DEFAULT '0',
   `waktu_pemilihan` datetime DEFAULT NULL,
-  `rt` int NOT NULL,
-  `rw` int NOT NULL
+  `rt` varchar(10) NOT NULL,
+  `rw` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user_detail`
 --
 
-LOCK TABLES `user_detail` WRITE;
-/*!40000 ALTER TABLE `user_detail` DISABLE KEYS */;
-INSERT INTO `user_detail` VALUES (41,93,'Vel libero ea impedi','Ut magna earum quibu','laki-laki','Debitis quia qui har','Facere consequatur ',1,NULL,66,43),(42,98,'Dolores ad eos nost','Asperiores et labore','laki-laki','Ea ut quos voluptate','Illo non cillum even',1,'2021-08-08 21:35:32',16,19),(44,78,'Consequuntur aut omn','Ut ut et placeat ve','perempuan','Cumque neque ad prae','Aliqua Aut quis ull',1,NULL,20,58);
-/*!40000 ALTER TABLE `user_detail` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `user_detail` (`user_id`, `nik`, `nama_lengkap`, `ttl`, `jenis_kelamin`, `alamat`, `pekerjaan`, `vote_status`, `waktu_pemilihan`, `rt`, `rw`) VALUES
+(41, 93, 'Vel libero ea impedi', 'Ut magna earum quibu', 'laki-laki', 'Debitis quia qui har', 'Facere consequatur ', 1, NULL, '66', '43'),
+(42, 98, 'Dolores ad eos nost', 'Asperiores et labore', 'laki-laki', 'Ea ut quos voluptate', 'Illo non cillum even', 1, '2021-08-08 21:35:32', '16', '19'),
+(44, 78, 'Consequuntur aut omn', 'Ut ut et placeat ve', 'perempuan', 'Cumque neque ad prae', 'Aliqua Aut quis ull', 1, NULL, '20', '58'),
+(45, 96, 'Illo exercitation ei', 'Quia consequat Do v', 'perempuan', 'Odio aut ab est nemo', 'Et ullamco lorem dol', 1, '2021-08-09 20:10:55', '1', '4'),
+(46, 89, 'Enim est consequuntu', 'Adipisicing deserunt', 'perempuan', 'Vitae deleniti aliqu', 'Et mollit qui incidu', 1, '2021-08-09 20:09:34', '001', '002'),
+(47, 29, 'Est doloribus ut pro', 'Totam enim nostrum s', 'laki-laki', 'Illo est sapiente i', 'Quas totam dolor fac', 1, '2021-08-09 20:15:45', '38', '25'),
+(48, 38, 'Excepteur voluptates', 'Tempor excepturi ut ', 'perempuan', 'Nostrud qui accusant', 'Tempora et aliquip t', 1, '2021-08-09 20:24:35', '87', '25'),
+(49, 59, 'Repellendus Invento', 'Consequatur In cons', 'laki-laki', 'In qui et amet accu', 'Eius nihil aliqua L', 1, '2021-08-09 21:50:20', '91', '56'),
+(50, 99, 'Vel qui magni iusto ', 'Dolor pariatur Quae', 'perempuan', 'Laborum aut ea aliqu', 'Ullamco est irure re', 1, '2021-08-09 21:52:05', '14', '22');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `user_menu`
 --
 
-DROP TABLE IF EXISTS `user_menu`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_menu` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `menu` varchar(128) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `id` int NOT NULL,
+  `menu` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `user_menu`
 --
 
-LOCK TABLES `user_menu` WRITE;
-/*!40000 ALTER TABLE `user_menu` DISABLE KEYS */;
-INSERT INTO `user_menu` VALUES (1,'Admin'),(2,'Vote'),(3,'User');
-/*!40000 ALTER TABLE `user_menu` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `user_menu` (`id`, `menu`) VALUES
+(1, 'Admin'),
+(2, 'Vote'),
+(3, 'User');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `user_role`
 --
 
-DROP TABLE IF EXISTS `user_role`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_role` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `role` varchar(128) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `id` int NOT NULL,
+  `role` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `user_role`
 --
 
-LOCK TABLES `user_role` WRITE;
-/*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
-INSERT INTO `user_role` VALUES (1,'Administrator'),(2,'User'),(4,'Kandidat');
-/*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `user_role` (`id`, `role`) VALUES
+(1, 'Administrator'),
+(2, 'User'),
+(4, 'Kandidat');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `user_sub_menu`
 --
 
-DROP TABLE IF EXISTS `user_sub_menu`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_sub_menu` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL,
   `menu_id` int NOT NULL,
   `title` varchar(128) NOT NULL,
   `url` varchar(128) NOT NULL,
   `icon` varchar(128) NOT NULL,
-  `is_active` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `is_active` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `user_sub_menu`
 --
 
-LOCK TABLES `user_sub_menu` WRITE;
-/*!40000 ALTER TABLE `user_sub_menu` DISABLE KEYS */;
-INSERT INTO `user_sub_menu` VALUES (1,1,'Dashboard','admin','fas fa-fw fa-tachometer-alt',1),(2,3,'My Profile','user','fas fa-fw fa-user',1),(3,3,'Edit Profile','user/edit','fas fa-fw fa-user-edit',1),(8,3,'Change Password','user/changepassword','fas fa-fw fa-key',1),(9,3,'Kelola Kandidat','kandidat','fas fa-user',1),(10,2,'Pemilihan','pemilihan','fas fa-comments',1),(11,3,'Kelola User','kelola-user','fas fa-fw fa-users',1),(12,3,'Kelola Panitia','kelola-panitia','fas fa-fw fa-user',0),(13,2,'Laporan','kelola-laporan','fas fa-fw fa-file',1);
-/*!40000 ALTER TABLE `user_sub_menu` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`) VALUES
+(1, 1, 'Dashboard', 'dashboard', 'fas fa-fw fa-tachometer-alt', 1),
+(2, 3, 'My Profile', 'user', 'fas fa-fw fa-user', 1),
+(3, 3, 'Edit Profile', 'user/edit', 'fas fa-fw fa-user-edit', 1),
+(8, 3, 'Change Password', 'user/changepassword', 'fas fa-fw fa-key', 1),
+(9, 3, 'Kelola Kandidat', 'kandidat', 'fas fa-user', 1),
+(10, 2, 'Pemilihan', 'pemilihan', 'fas fa-comments', 1),
+(11, 3, 'Kelola User', 'kelola-user', 'fas fa-fw fa-users', 1),
+(12, 3, 'Kelola Panitia', 'kelola-panitia', 'fas fa-fw fa-user', 0),
+(13, 2, 'Laporan', 'kelola-laporan', 'fas fa-fw fa-file', 1);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `vote`
 --
 
-DROP TABLE IF EXISTS `vote`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `vote` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL,
   `user_id` int NOT NULL,
   `pilihan` int NOT NULL,
-  `periode` year NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `periode` year NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `vote`
 --
 
-LOCK TABLES `vote` WRITE;
-/*!40000 ALTER TABLE `vote` DISABLE KEYS */;
-INSERT INTO `vote` VALUES (2,41,4,2021),(3,42,5,2021);
-/*!40000 ALTER TABLE `vote` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `vote` (`id`, `user_id`, `pilihan`, `periode`) VALUES
+(3, 42, 5, 2021),
+(4, 46, 5, 2021),
+(5, 45, 4, 2021),
+(6, 47, 5, 2021),
+(7, 48, 4, 2021),
+(8, 49, 5, 2021);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kandidat`
+--
+ALTER TABLE `kandidat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_menu`
+--
+ALTER TABLE `user_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `vote`
+--
+ALTER TABLE `vote`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kandidat`
+--
+ALTER TABLE `kandidat`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+
+--
+-- AUTO_INCREMENT for table `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `user_menu`
+--
+ALTER TABLE `user_menu`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `vote`
+--
+ALTER TABLE `vote`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-08-08 22:05:47
